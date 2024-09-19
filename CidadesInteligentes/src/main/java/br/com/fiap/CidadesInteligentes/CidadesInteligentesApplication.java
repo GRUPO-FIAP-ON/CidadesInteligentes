9e12f6198451d2eff91b2350f7a49dfdb53c1d76
Thábata Orbeteli <125618214+thabataorbeteli@users.noreply.github.com>
2024-09-18 22:57:33 -0300

    Add files via upload

---
+++ CidadesInteligentes/src/main/java/br/com/fiap/CidadesInteligentes/CidadesInteligentesApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.CidadesInteligentes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CidadesInteligentesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CidadesInteligentesApplication.class, args);
+	}
+
+}
