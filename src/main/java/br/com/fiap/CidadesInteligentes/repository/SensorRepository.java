fe48e3e2970d6069532c1edec978a666966a534d
thabataorbeteli <rm550268@fiap.com.br>
2024-09-18 23:05:01 -0300

    update inicial

--- src/main/java/br/com/fiap/CidadesInteligentes/repository/SensorRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.CidadesInteligentes.repository;
-
-import br.com.fiap.CidadesInteligentes.model.Sensor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SensorRepository extends JpaRepository<Sensor, Long> {
-}
--- src/main/java/br/com/fiap/CidadesInteligentes/repository/StreetLightRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.CidadesInteligentes.repository;
-
-import br.com.fiap.CidadesInteligentes.model.StreetLight;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StreetLightRepository extends JpaRepository<StreetLight, Long> {
-}
